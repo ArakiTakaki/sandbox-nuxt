8234d9d2d608c64dae50a8387045b3ee7a5fa4a6
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-09-11 02:12:14 +0900

    🚀 : initial commit

---
+++ pages/sample.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div>test</div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator"
+import { State } from "vuex-class"
+// import Card from "~/components/Card.vue"
+
+@Component({})
+export default class extends Vue {
+}
+</script>
+
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
